16db4996a7aa4dab58cddf36b8377c9f541735d3
Brendon Gomes <brendongomes40@gmail.com>
2024-11-15 22:43:57 -0300

    add new migration: other entities (seleler, salesrecord)

---
+++ SalesManagementWebMvcSystem/Models/Enums/SalesStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesManagementWebMvcSystem.Models.Enums
+{
+    public enum SalesStatus : int
+    {
+        Pending = 0, // Pendente
+        Billed = 1, // Faturado
+        Canceled = 2 // Cancelado
+    }
+}
