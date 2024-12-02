313a8dc14eee4b7f85e4bf82099d72431ed1ffd2
Brendon Gomes <brendongomes40@gmail.com>
2024-12-01 22:35:31 -0300

    Preparing sales search navigation views

---
+++ SalesManagementWebMvcSystem/Controllers/SalesRecordsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesManagementWebMvcSystem.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
