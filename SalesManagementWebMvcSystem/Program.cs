fdbc3ba8546e5630d1fbec43b78a47dfe4e4e6de
Brendon Gomes <brendongomes40@gmail.com>
2024-12-01 22:49:06 -0300

    Implementing simple search

--- SalesManagementWebMvcSystem/Program.cs
+++ SalesManagementWebMvcSystem/Program.cs
@@ -23,6 +23,7 @@ namespace SalesManagementWebMvcSystem
             builder.Services.AddScoped<SeedingService>();
             builder.Services.AddScoped<SellerService>();
             builder.Services.AddScoped<DepartmentService>();
+            builder.Services.AddScoped<SalesRecordService>();
 
             var app = builder.Build();
 
---
+++ SalesManagementWebMvcSystem/Services/SalesRecordService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesManagementWebMvcSystem.Data;
+using SalesManagementWebMvcSystem.Models;
+
+namespace SalesManagementWebMvcSystem.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesManagementWebMvcSystemContext _context;
+
+        public SalesRecordService(SalesManagementWebMvcSystemContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+                result = result.Where(x => x.Date >= minDate.Value);
+            if (maxDate.HasValue)
+                result = result.Where(x => x.Date <= maxDate.Value);
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
